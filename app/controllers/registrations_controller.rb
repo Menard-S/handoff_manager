0c949284ad6e31aa853fb2b1d65e4380344dcb2b
msegfilph <menard.seguenza@gmail.com>
2024-03-09 15:40:11 +0800

    Resolves  undefined callback error for require_user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,30 +1,29 @@
 class RegistrationsController < ApplicationController
-  # skip_before_action :require_user, only: [:new, :create]
-    def new
-        @user = User.new
-    end
-
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-          redirect_to dashboard_path, notice: "User created successfully."
-        else
-          respond_to do |format|
-            format.html { render :new, status: :unprocessable_entity }
-            format.turbo_stream do
-              render turbo_stream: turbo_stream.replace("form_errors", partial: "layouts/form_errors", locals: { user: @user }), 
-                     status: :unprocessable_entity
+      def new
+          @user = User.new
+      end
+  
+      def create
+          @user = User.new(user_params)
+          if @user.save
+              session[:user_id] = @user.id
+            redirect_to dashboard_path, notice: "User created successfully."
+          else
+            respond_to do |format|
+              format.html { render :new, status: :unprocessable_entity }
+              format.turbo_stream do
+                render turbo_stream: turbo_stream.replace("form_errors", partial: "layouts/form_errors", locals: { user: @user }), 
+                       status: :unprocessable_entity
+              end
             end
           end
         end
+        
+        
+  
+      private
+  
+      def user_params
+          params.require(:user).permit(:email, :password, :password_confirmation)
       end
-      
-      
-
-    private
-
-    def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
-    end
-end
+  end
